abe4dad0c654298ebfcd70c14b855ca051e9e4f6
vothanhhai1996 <haivt1@f88.vn>
2020-12-08 17:30:04 +0700

    update name

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -25,8 +25,8 @@ const About = () => {
               <SubTitle> Front End Developer</SubTitle>
             </div> 
             <div>
-              <Title> Hello, I’m AbdAli </Title>
-              <Text> I'm a digital Front End Developer hailing from <b className="text-primary lined-link">North Africa</b> living in Casablanca. </Text>
+              <Title> Hello, I’m Hai </Title>
+              <Text> I'm a Mobile Developer from <b className="text-primary lined-link">Hồ Chí Minh</b> living in 11 district. </Text>
               <Text> I love working with modern technologies, building and designing awesome projects. I prefer minimalistic & clean designs with strong user experience.</Text>
               <Text> behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.
               </Text>
